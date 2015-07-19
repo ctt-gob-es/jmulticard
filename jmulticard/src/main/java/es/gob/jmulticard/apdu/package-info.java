0a55e2769342401ab33f093da72df66a392b185f
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2015-07-19 16:24:14 +0000

    Se migran un par de package.html de Java 5 a package-info.java de Java 6
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/jmulticard/trunk@6171 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ jmulticard/src/main/java/es/gob/jmulticard/apdu/package-info.java
@@ -0,0 +1,5 @@
+/** Clases para el tratamiento de las APDU individualizadas y representadas en forma de objetos (as&iacute; como clases de utilidad).
+ *  A su vez se divide en distintos paquetes correspondientes a las normativas relacionadas con las APDU implementadas,
+ *  como ISO-7816-4 o las propias de cada tarjeta.
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s. */
+package es.gob.jmulticard.apdu;
---
+++ jmulticard/src/main/java/es/gob/jmulticard/package-info.java
@@ -0,0 +1,3 @@
+/** Paquete principal del proyecto JMultiCard.
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s. */
+package es.gob.jmulticard;
