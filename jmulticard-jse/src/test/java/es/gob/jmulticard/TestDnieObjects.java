a7e48f6dad13fd2c9a2cf910bb65b7788b4820ae
ClawGrip <clawgrip@hotmail.com>
2016-07-13 15:34:13 +0200

    Avance en la implementación de los DG del DNIe 3.0

--- jmulticard-jse/src/test/java/es/gob/jmulticard/TestDnieObjects.java
+++ jmulticard-jse/src/test/java/es/gob/jmulticard/TestDnieObjects.java
@@ -1,8 +1,15 @@
 package es.gob.jmulticard;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import org.junit.Test;
 
 import es.gob.jmulticard.apdu.connection.ApduConnection;
+import es.gob.jmulticard.asn1.Tlv;
 import es.gob.jmulticard.card.Location;
 import es.gob.jmulticard.card.dnie.Dnie3;
 import es.gob.jmulticard.card.dnie.Dnie3Dg01Mrz;
@@ -14,18 +21,31 @@ import es.gob.jmulticard.jse.smartcardio.SmartcardIoConnection;
  * @author Sergio Mart&iacute;nez Rico. */
 public final class TestDnieObjects {
 
-    private static final byte[] ID_FILE_3F01 = new byte[] { (byte) 0x3f, (byte) 0x1 };
-    private static final byte[] ID_FILE_DG02 = new byte[] { (byte) 0x01, (byte) 0x2 };
-    private static final byte[] ID_FILE_DG07 = new byte[] { (byte) 0x01, (byte) 0x7 };
-    private static final byte[] ID_FILE_DG11 = new byte[] { (byte) 0x01, (byte) 0x0 };
+    private static final Location FILE_DG01_LOCATION_MRZ   = new Location("3F010101"); //$NON-NLS-1$
+    private static final Location FILE_DG02_LOCATION_PHOTO = new Location("3F010102"); //$NON-NLS-1$
+    private static final Location FILE_DG07_LOCATION_SIGN  = new Location("3F010107"); //$NON-NLS-1$
 
-    private static final Location FILE_DG01_LOCATION_MRZ = new Location("3F010101"); //$NON-NLS-1$
+    /** No vamos a leer este fichero, todo lo que da esta en el MRZ. */
+    private static final Location FILE_DG11_LOCATION_DATA  = new Location("3F01010B"); //$NON-NLS-1$
+
+    private static final String JPEG2K_HEADER = "0000000C6A502020"; //$NON-NLS-1$
+
+    private static final BufferedImage extractImage(final byte[] photo) throws IOException {
+    	if (photo == null) {
+    		throw new IllegalArgumentException("Los datos de entrada no pueden ser nulos"); //$NON-NLS-1$
+    	}
+    	final int headerSize = (HexUtils.hexify(photo, false).indexOf(JPEG2K_HEADER) / 2);
+    	final byte[] pj2kPhoto = new byte[photo.length - headerSize];
+        System.arraycopy(photo, headerSize, pj2kPhoto, 0, pj2kPhoto.length);
+        return ImageIO.read(new ByteArrayInputStream(pj2kPhoto));
+    }
 
     public static void main(final String args[]) throws Exception {
     	new TestDnieObjects().testReadDnieObjects();
     }
 
-	/** Prueba la obtenci&oacute;n de los objetos del DNIe */
+	/** Prueba la obtenci&oacute;n de los objetos del DNIe
+	 * @throws Exception En cualquier error. */
     @SuppressWarnings("static-method")
 	@Test
 	public void testReadDnieObjects() throws Exception {
@@ -44,46 +64,18 @@ public final class TestDnieObjects {
         System.out.println(new Dnie3Dg01Mrz(mrz).getNationality());
         System.out.println(new Dnie3Dg01Mrz(mrz).getSex());
 
-//		//Lectura de la imagen del propietario del DNIe
-//		file = getDnieFileBytes(ID_FILE_DG2, 117, conn);
-//
-//        try {
-//        	//Se debe modificar la clase DG2 para quitar los caracteres de encapsulacion y dejarlo en formato jp2
-//			final InputStream in = new ByteArrayInputStream(new DG2(file.toByteArray()).getImageBytes());
-//			final BufferedImage bImageFromConvert = ImageIO.read(in);
-//			System.out.println(bImageFromConvert);
-//        }
-//		catch(final Exception e) {
-//	    	System.out.println("Error al obtener la imagen del propietario del DNIe"); //$NON-NLS-1$
-//	    }
-//
-//        //Para el DG3 se obtiene 69-82, no tenemos permisos suficientes
-//
-//        //Lectura de la imagen de firma
-//        file = getDnieFileBytes(ID_FILE_DG7, 101, conn);
-//        try {
-//        	//Se debe modificar la clase DG7 para quitar los caracteres de encapsulacion y dejarlo en formato jp2
-//        	final InputStream in = new ByteArrayInputStream(file.toByteArray());
-//        	final BufferedImage bImageFromConvert = ImageIO.read(in);
-//        	System.out.println(bImageFromConvert);
-//        }
-//        catch(final Exception e) {
-//        	System.out.println("Error al obtener la imagen de firma"); //$NON-NLS-1$
-//        }
-//
-//		//Lectura de datos adicionales
-//        file = getDnieFileBytes(ID_FILE_DG11, 107, conn);
-//
-//        try {
-//        	//Necesaria mejora de la clase DG11 para obtener los campos
-//        	System.out.println(new DG11(file.toByteArray()).getAddress(3));
-//	        System.out.println(new DG11(file.toByteArray()).getCustodyInfo());
-//	        System.out.println(new DG11(file.toByteArray()).getPersonalNumber());
-//	        System.out.println(new DG11(file.toByteArray()).getPhone());
-//	        System.out.println(new DG11(file.toByteArray()).getProfession());
-//        }
-//        catch(final Exception e) {
-//        	System.out.println("Error al obtener los datos adicionales"); //$NON-NLS-1$
-//        }
+        final byte[] photo = dni.selectFileByLocationAndRead(FILE_DG02_LOCATION_PHOTO);
+        System.out.println(extractImage(photo));
+
+        final byte[] sign = dni.selectFileByLocationAndRead(FILE_DG07_LOCATION_SIGN);
+        System.out.println(extractImage(sign));
+
+        final byte[] data = dni.selectFileByLocationAndRead(FILE_DG11_LOCATION_DATA);
+        final Tlv tlv = new Tlv(data);
+        System.out.println("TLV: " + HexUtils.hexify(tlv.getValue(), false));
+        final Tlv tlv2 = new Tlv(tlv.getValue());
+        System.out.println("TAG: " + tlv2.getTag());
+        System.out.println("VAL: " + HexUtils.hexify(tlv2.getValue(), false));
+
     }
 }
