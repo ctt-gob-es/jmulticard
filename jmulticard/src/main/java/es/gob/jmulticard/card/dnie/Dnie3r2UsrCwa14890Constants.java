a41b85d48e0c12e072df38e62d73876ed9b87f7c
ClawGrip <clawgrip@hotmail.com>
2020-09-15 18:07:31 +0200

    Consolidación de las constantes CWA14890 del DNIe v3

---
+++ jmulticard/src/main/java/es/gob/jmulticard/card/dnie/Dnie3r2UsrCwa14890Constants.java
@@ -0,0 +1,79 @@
+/*
+ * Controlador Java de la Secretaria de Estado de Administraciones Publicas
+ * para el DNI electronico.
+ *
+ * El Controlador Java para el DNI electronico es un proveedor de seguridad de JCA/JCE
+ * que permite el acceso y uso del DNI electronico en aplicaciones Java de terceros
+ * para la realizacion de procesos de autenticacion, firma electronica y validacion
+ * de firma. Para ello, se implementan las funcionalidades KeyStore y Signature para
+ * el acceso a los certificados y claves del DNI electronico, asi como la realizacion
+ * de operaciones criptograficas de firma con el DNI electronico. El Controlador ha
+ * sido disenado para su funcionamiento independiente del sistema operativo final.
+ *
+ * Copyright (C) 2012 Direccion General de Modernizacion Administrativa, Procedimientos
+ * e Impulso de la Administracion Electronica
+ *
+ * Este programa es software libre y utiliza un licenciamiento dual (LGPL 2.1+
+ * o EUPL 1.1+), lo cual significa que los usuarios podran elegir bajo cual de las
+ * licencias desean utilizar el codigo fuente. Su eleccion debera reflejarse
+ * en las aplicaciones que integren o distribuyan el Controlador, ya que determinara
+ * su compatibilidad con otros componentes.
+ *
+ * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la
+ * Lesser GNU General Public License publicada por la Free Software Foundation,
+ * tanto en la version 2.1 de la Licencia, o en una version posterior.
+ *
+ * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la
+ * European Union Public License publicada por la Comision Europea,
+ * tanto en la version 1.1 de la Licencia, o en una version posterior.
+ *
+ * Deberia recibir una copia de la GNU Lesser General Public License, si aplica, junto
+ * con este programa. Si no, consultelo en <http://www.gnu.org/licenses/>.
+ *
+ * Deberia recibir una copia de la European Union Public License, si aplica, junto
+ * con este programa. Si no, consultelo en <http://joinup.ec.europa.eu/software/page/eupl>.
+ *
+ * Este programa es distribuido con la esperanza de que sea util, pero
+ * SIN NINGUNA GARANTIA; incluso sin la garantia implicita de comercializacion
+ * o idoneidad para un proposito particular.
+ */
+package es.gob.jmulticard.card.dnie;
+
+/** Constantes del DNIe para el establecimiento de canal seguro CWA-14890.
+ * @author Carlos Gamuci
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s
+ * @author Alberto Mart&iacute;nez. */
+class Dnie3r2UsrCwa14890Constants extends Dnie3UsrCwa14890Constants {
+
+    /** Certificado de la CA intermedia de Terminal verificable por la tarjeta.
+     * (<i>c-CV-CA-CS-AUT</i>). */
+	private static final byte[] C_CV_CA_R2 = new byte[] {
+        (byte) 0x7F, (byte) 0x21, (byte) 0x81, (byte) 0xCE, (byte) 0x5F, (byte) 0x37, (byte) 0x81, (byte) 0x80, (byte) 0x3C, (byte) 0xBA,
+        (byte) 0xDC, (byte) 0x36, (byte) 0x84, (byte) 0xBE, (byte) 0xF3, (byte) 0x20, (byte) 0x41, (byte) 0xAD, (byte) 0x15, (byte) 0x50,
+        (byte) 0x89, (byte) 0x25, (byte) 0x8D, (byte) 0xFD, (byte) 0x20, (byte) 0xC6, (byte) 0x91, (byte) 0x15, (byte) 0xD7, (byte) 0x2F,
+        (byte) 0x9C, (byte) 0x38, (byte) 0xAA, (byte) 0x99, (byte) 0xAD, (byte) 0x6C, (byte) 0x1A, (byte) 0xED, (byte) 0xFA, (byte) 0xB2,
+        (byte) 0xBF, (byte) 0xAC, (byte) 0x90, (byte) 0x92, (byte) 0xFC, (byte) 0x70, (byte) 0xCC, (byte) 0xC0, (byte) 0x0C, (byte) 0xAF,
+        (byte) 0x48, (byte) 0x2A, (byte) 0x4B, (byte) 0xE3, (byte) 0x1A, (byte) 0xFD, (byte) 0xBD, (byte) 0x3C, (byte) 0xBC, (byte) 0x8C,
+        (byte) 0x83, (byte) 0x82, (byte) 0xCF, (byte) 0x06, (byte) 0xBC, (byte) 0x07, (byte) 0x19, (byte) 0xBA, (byte) 0xAB, (byte) 0xB5,
+        (byte) 0x6B, (byte) 0x6E, (byte) 0xC8, (byte) 0x07, (byte) 0x60, (byte) 0xA4, (byte) 0xA9, (byte) 0x3F, (byte) 0xA2, (byte) 0xD7,
+        (byte) 0xC3, (byte) 0x47, (byte) 0xF3, (byte) 0x44, (byte) 0x27, (byte) 0xF9, (byte) 0xFF, (byte) 0x5C, (byte) 0x8D, (byte) 0xE6,
+        (byte) 0xD6, (byte) 0x5D, (byte) 0xAC, (byte) 0x95, (byte) 0xF2, (byte) 0xF1, (byte) 0x9D, (byte) 0xAC, (byte) 0x00, (byte) 0x53,
+        (byte) 0xDF, (byte) 0x11, (byte) 0xA5, (byte) 0x07, (byte) 0xFB, (byte) 0x62, (byte) 0x5E, (byte) 0xEB, (byte) 0x8D, (byte) 0xA4,
+        (byte) 0xC0, (byte) 0x29, (byte) 0x9E, (byte) 0x4A, (byte) 0x21, (byte) 0x12, (byte) 0xAB, (byte) 0x70, (byte) 0x47, (byte) 0x58,
+        (byte) 0x8B, (byte) 0x8D, (byte) 0x6D, (byte) 0xA7, (byte) 0x59, (byte) 0x22, (byte) 0x14, (byte) 0xF2, (byte) 0xDB, (byte) 0xA1,
+        (byte) 0x40, (byte) 0xC7, (byte) 0xD1, (byte) 0x22, (byte) 0x57, (byte) 0x9B, (byte) 0x5F, (byte) 0x38, (byte) 0x3D, (byte) 0x22,
+        (byte) 0x53, (byte) 0xC8, (byte) 0xB9, (byte) 0xCB, (byte) 0x5B, (byte) 0xC3, (byte) 0x54, (byte) 0x3A, (byte) 0x55, (byte) 0x66,
+        (byte) 0x0B, (byte) 0xDA, (byte) 0x80, (byte) 0x94, (byte) 0x6A, (byte) 0xFB, (byte) 0x05, (byte) 0x25, (byte) 0xE8, (byte) 0xE5,
+        (byte) 0x58, (byte) 0x6B, (byte) 0x4E, (byte) 0x63, (byte) 0xE8, (byte) 0x92, (byte) 0x41, (byte) 0x49, (byte) 0x78, (byte) 0x36,
+        (byte) 0xD8, (byte) 0xD3, (byte) 0xAB, (byte) 0x08, (byte) 0x8C, (byte) 0xD4, (byte) 0x4C, (byte) 0x21, (byte) 0x4D, (byte) 0x6A,
+        (byte) 0xC8, (byte) 0x56, (byte) 0xE2, (byte) 0xA0, (byte) 0x07, (byte) 0xF4, (byte) 0x4F, (byte) 0x83, (byte) 0x74, (byte) 0x33,
+        (byte) 0x37, (byte) 0x37, (byte) 0x1A, (byte) 0xDD, (byte) 0x8E, (byte) 0x03, (byte) 0x00, (byte) 0x01, (byte) 0x00, (byte) 0x01,
+        (byte) 0x42, (byte) 0x08, (byte) 0x65, (byte) 0x73, (byte) 0x52, (byte) 0x44, (byte) 0x49, (byte) 0x60, (byte) 0x00, (byte) 0x06
+    };
+
+	@Override
+	public byte[] getCCvCa() {
+		return C_CV_CA_R2;
+	}
+
+}
