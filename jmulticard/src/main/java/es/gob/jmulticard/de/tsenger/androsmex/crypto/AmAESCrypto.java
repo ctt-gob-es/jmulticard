a4b5873323f874e44a08428631bfea8074c54cc7
ClawGrip <clawgrip@hotmail.com>
2022-03-01 12:28:09 +0100

    Ahora todos los descifrados AES se hacen desde CryptoHelper
    ...Y no desde AmAESCrypto, que ahora es una mera pasarela

--- jmulticard/src/main/java/es/gob/jmulticard/de/tsenger/androsmex/crypto/AmAESCrypto.java
+++ jmulticard/src/main/java/es/gob/jmulticard/de/tsenger/androsmex/crypto/AmAESCrypto.java
@@ -19,10 +19,7 @@
 
 package es.gob.jmulticard.de.tsenger.androsmex.crypto;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
@@ -34,14 +31,8 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.spongycastle.crypto.BufferedBlockCipher;
-import org.spongycastle.crypto.engines.AESEngine;
-import org.spongycastle.crypto.modes.CBCBlockCipher;
 import org.spongycastle.crypto.paddings.BlockCipherPadding;
 import org.spongycastle.crypto.paddings.ISO7816d4Padding;
-import org.spongycastle.crypto.paddings.PaddedBufferedBlockCipher;
-import org.spongycastle.crypto.params.KeyParameter;
-import org.spongycastle.crypto.params.ParametersWithIV;
 import org.spongycastle.jce.provider.BouncyCastleProvider;
 
 import es.gob.jmulticard.CryptoHelper;
@@ -148,82 +139,39 @@ public final class AmAESCrypto {
 	/** Desencripta datos (el algoritmo depende de la implementaci&oacute;n
 	 * concreta de la clase).
 	 * @param in Datos cifrados.
-	 * @param key Clave de descifrado.
+	 * @param aesKey Clave de descifrado.
 	 * @param ssc Contador de secuencia de env&iacute;os (<i>Send Sequence Counter</i>).
 	 * @param ch Utilidad para las operaciones criptogr&aacute;ficas.
 	 * @return Datos descifrados (en claro).
 	 * @throws AmCryptoException En cualquier error. */
 	public static byte[] decrypt(final byte[] in,
-                                 final byte[] key,
+                                 final byte[] aesKey,
                                  final byte[] ssc,
                                  final CryptoHelper ch) throws AmCryptoException {
 
-		int noBytesRead = 0; // Numero de octetos leidos de la entrada
-		int noBytesProcessed = 0; // Numero de octetos procesados
-
-		final BufferedBlockCipher decryptCipher = new PaddedBufferedBlockCipher(
-			new CBCBlockCipher(
-				new AESEngine()
-			),
-			ISO7816D4_PADDING
-		);
-		// Creamos los parametros de descifrado con el vector de inicializacion (iv)
-		final ParametersWithIV parameterIV;
 		try {
-			parameterIV = new ParametersWithIV(
-				new KeyParameter(key),
-				encryptBlock(key, ssc)
+			return ch.aesDecrypt(
+				in,
+				encryptBlock(aesKey, ssc),
+				aesKey,
+				"ISO7816-4Padding" //$NON-NLS-1$
 			);
 		}
 		catch (final InvalidKeyException       |
-			         NoSuchAlgorithmException  |
-			         NoSuchPaddingException    |
-			         IllegalBlockSizeException |
-			         BadPaddingException e1) {
+		             NoSuchAlgorithmException  |
+		             NoSuchPaddingException    |
+		             IllegalBlockSizeException |
+		             BadPaddingException e1) {
 			throw new AmCryptoException(
 				"Error creando el vector de inicializacion AES mediante un cifrado de bloque AES: " + e1, e1 //$NON-NLS-1$
 			);
 		}
-		// Inicializamos
-		decryptCipher.init(false, parameterIV);
-
-		// Buffers para mover octetos de un flujo a otro
-		final byte[] buf = new byte[16]; // Buffer de entrada
-		final byte[] obuf = new byte[512]; // Buffer de salida
-
-		try (
-			final InputStream bin = new ByteArrayInputStream(in);
-			final ByteArrayOutputStream bout = new ByteArrayOutputStream()
-		) {
-			try {
-				while ((noBytesRead = bin.read(buf)) >= 0) {
-					noBytesProcessed = decryptCipher.processBytes(
-						buf,
-						0,
-						noBytesRead,
-						obuf,
-						0
-					);
-					bout.write(obuf, 0, noBytesProcessed);
-				}
-			}
-			catch (final Exception e) {
-				throw new AmCryptoException(e);
-			}
-
-			try {
-				noBytesProcessed = decryptCipher.doFinal(obuf, 0);
-				bout.write(obuf, 0, noBytesProcessed);
-				bout.flush();
-				return bout.toByteArray();
-			}
-			catch (final Exception e) {
-				throw new AmCryptoException(e);
-			}
+		catch (final IOException e) {
+			throw new AmCryptoException(
+				"Error en el cifrado AES: " + e, e //$NON-NLS-1$
+			);
 		}
-		catch (final IOException ioe) {
-			throw new AmCryptoException(ioe);
-		}
+
 	}
 
 	/** A&ntilde;ade un relleno ISO9797-1 (m&eacute;todo 2) / ISO7816d4-Padding
