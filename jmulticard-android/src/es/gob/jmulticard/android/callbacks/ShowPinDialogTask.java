77b7ee8512a31a36284dfbda1eb04e2210e7e4d8
Carlos Gamuci <carlos.gamuci@gmail.com>
2022-07-04 17:18:09 +0200

    Cambio visibilidad y corrección compilación
    - Se hace público el diálogo de solicitud de PIN.
    - Se evita que se agreguen a los JAR los package-info.class para evitar problemas de compilación de los proyectos Android que dependan de este módulo.

--- jmulticard-android/src/es/gob/jmulticard/android/callbacks/ShowPinDialogTask.java
+++ jmulticard-android/src/es/gob/jmulticard/android/callbacks/ShowPinDialogTask.java
@@ -1,23 +1,25 @@
 package es.gob.jmulticard.android.callbacks;
 
-import java.util.logging.Logger;
-
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import es.gob.jmulticard.android.nfc.AndroidNfcConnection;
 
 /**
  * Created by a621914 on 08/06/2016.
  */
 
-class ShowPinDialogTask extends AsyncTask<String, String, String> {
+public class ShowPinDialogTask extends AsyncTask<String, String, String> {
+
+    private static final String TAG = AndroidNfcConnection.class.getSimpleName();
 
     final PinDialog dialog;
 	final FragmentTransaction ft;
 	private static Activity activity;
 	static DialogDoneChecker dialogDone;
 
-	ShowPinDialogTask(final PinDialog dialog, final FragmentTransaction ft, final Activity act, final DialogDoneChecker ddc) {
+	public ShowPinDialogTask(final PinDialog dialog, final FragmentTransaction ft, final Activity act, final DialogDoneChecker ddc) {
     	this.dialog = dialog;
     	this.ft = ft;
     	activity = act;
@@ -54,7 +56,7 @@ class ShowPinDialogTask extends AsyncTask<String, String, String> {
 	    }
 	    catch (final InterruptedException e)
 	    {
-	    	Logger.getLogger("es.gob.jmulticard").info("Error en la espera a la introduccion de PIN: " + e); //$NON-NLS-1$ //$NON-NLS-2$
+	    	Log.w(TAG, "Error en la espera a la introduccion de PIN: " + e); //$NON-NLS-1$
 	    }
 
 		this.m_Input = this.dialog.getPassword();
