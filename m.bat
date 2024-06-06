e02166858e0aef0a7dcde03d303650156e2a4136
Carlos Gamuci <carlos.gamuci@gmail.com>
2024-06-06 12:51:21 +0200

    Cambio a versión estable 1.9
    - Se actualiza la versión en los pom.xml para reflejar la 1.9.

--- m.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-set JAVA_HOME=C:\java\OpenJDK8
-set PATH=C:\java\OpenJDK8\bin;C:\java\maven\bin;%PATH%
-C:\java\maven\bin\mvn.cmd clean install
--- setenv.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set JAVA_HOME=c:\java\JDK19
-set path=c:\java\maven\bin;c:\java\JDK19\bin;%path%
