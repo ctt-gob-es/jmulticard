9334d12ca9ef7d228b5cdb9f3a501ac3b064cc56
José Montero <jose.montero@ricoh.es>
2025-07-24 12:43:34 +0200

    - Comentarios para CardWithRetriesPasswordCallback

--- jmulticard/src/main/java/es/gob/jmulticard/callback/CardWithRetriesPasswordCallback.java
+++ jmulticard/src/main/java/es/gob/jmulticard/callback/CardWithRetriesPasswordCallback.java
@@ -2,20 +2,32 @@ package es.gob.jmulticard.callback;
 
 import javax.security.auth.callback.PasswordCallback;
 
+/** <i>Callback</i> de solicitud de PIN con intentos restantes para tarjetas.
+ * <code>javax.security.auth.callback.PasswordCallback</code>. */
 public class CardWithRetriesPasswordCallback extends PasswordCallback {
 
 	private static final long serialVersionUID = 7929236918907338858L;
 	
+	/** Intentos restantes para la tarjeta */
 	private int retriesLeft = 0;
 
+	
 	public CardWithRetriesPasswordCallback(final String prompt, final boolean echoOn) {
 		super(prompt, echoOn);
 	}
 
+	/**
+	 * Asigna los intentos que le quedan a la tarjeta.
+	 * @param retriesLeft Intentos restantes.
+	 */
 	public void setRetriesLeft(final int retriesLeft) {
 		this.retriesLeft = retriesLeft;
 	}
 
+	/**
+	 * Devuelve los intentos restantes para la tarjeta.
+	 * @return Intentos restantes.
+	 */
 	public int getRetriesLeft() {
 		return this.retriesLeft;
 	}
